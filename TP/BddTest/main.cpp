520d21a72596986030c04fb7073096386a4f5907
root <lamootalexandre@gmail.com>
2015-11-20 14:04:20 +0100

    add test bdd

---
+++ TP/BddTest/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+#include "DataBaseType.h"
+#include "MySQL.h"
+#include "SQLite.h"
+using namespace std;
+int main()
+{
+    cout << "Hello world!" << endl;
+    db::DataBaseType<MySQL> dbMySQL("mycloud", "mycloud", "mycloud") ;
+    string idMySQL = dbMySQL.getUser("root", "");
+    cout << "Test MySQL" << endl << "idMySQL : " << idMySQL << endl;
+
+    /*db::DataBaseType<SQLite> dbSQLite("/home/web2014/temp/mycloud.sqlite3") ;
+    string idSQLite = dbSQLite.getUser("root", "");
+    cout << "Test SQLite" << endl << "idSQLite : " << idSQLite << endl;*/
+    return 0;
+}
